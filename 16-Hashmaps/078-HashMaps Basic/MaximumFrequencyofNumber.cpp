341d359d654ead0aebd6263483f31ed5647cd4e4
Dhruvesh Jayesh Patel <dhruveshpatel-cmpn@atharvacoe.ac.in>
2025-01-23 23:47:13 +0530

    Today's work done

--- 16-Hashmaps/078-HashMaps Basic/MaximumFrequencyofNumber.cpp
+++ 16-Hashmaps/078-HashMaps Basic/MaximumFrequencyofNumber.cpp
@@ -1,12 +1,11 @@
 /*Maximum Frequency Number
 Ninja is given an array of integers that contain numbers in random order. He needs to write a program to find and return the number which occurs the maximum times in the given input. He needs your help to solve this problem.
-
 If two or more elements contend for the maximum frequency, return the element which occurs in the array first i.e. whose index is lowest.
 
 For example,
 
 For 'arr' = [ 1, 2, 3, 1, 2]. you need to return 1.
-https://www.naukri.com/code360/problems/maximum-frequency-number_920319?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar*/
+https://www.naukri.com/code360/problems/maximum-frequency-number_920319*/
 #include<iostream>
 #include<map>
 #include<unordered_map>
