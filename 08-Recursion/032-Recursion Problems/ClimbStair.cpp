b9061d2ee9fe7664b559802e4aedf3fc75329a30
Dhruvesh Jayesh Patel <dhruveshpatel-cmpn@atharvacoe.ac.in>
2025-01-23 22:57:46 +0530

    Today's work done

--- 08-Recursion/032-Recursion Problems/ClimbStair.cpp
+++ 08-Recursion/032-Recursion Problems/ClimbStair.cpp
@@ -2,7 +2,7 @@
 You have been given a number of stairs. Initially, you are at the 0th stair, and you need to reach the Nth stair.
 Each time, you can climb either one step or two steps.
 You are supposed to return the number of distinct ways you can climb from the 0th step to the Nth step.
-https://www.naukri.com/code360/problems/count-ways-to-reach-nth-stairs_798650?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar*/
+https://www.naukri.com/code360/problems/count-ways-to-reach-nth-stairs_798650*/
 #include<iostream>
 using namespace std;
 
